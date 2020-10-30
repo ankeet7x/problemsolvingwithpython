c8fccfd007becbfd672b6edf02fc45ed23084dcd
ankeetkarki <ankeetkarki30@gmail.com>
2020-10-30 16:15:40 +0545

    Global and country's cases' details can be gained

--- Coronavirus update on terminal/covid.py
+++ Coronavirus update on terminal/covid.py
@@ -2,28 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = 'https://www.worldometers.info/coronavirus/'
-
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-list = []
-yo = soup.find_all(class_='maincounter-number')
-
-# data = pd.DataFrame({
-#     'yo':yo
-# })
-for cases in yo:
-    list.append(cases.get_text())
-
-totalCases, totalDeaths, totalRecovered = list
-
-print(f'total cases: {totalCases}')
-print(f'total deaths: {totalDeaths}')
-print(f'total recovered: {totalRecovered}')
 
 
-# print(data)
-# print(yo)
+def covidInquiry(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-# print(yo.get_text())
+    list = []
+    allData = soup.find_all(class_='maincounter-number')
+
+
+    for cases in allData:
+        list.append(cases.get_text())
+
+    totalCases, totalDeaths, totalRecovered = list
+
+    print(f'total cases: {totalCases}')
+    print(f'total deaths: {totalDeaths}')
+    print(f'total recovered: {totalRecovered}')
+
+
+print("Do you want to view global data or country data ?\n")
+check = input("Type global for global data or country name for country data\n")
+
+if check == 'global':
+    url = 'https://www.worldometers.info/coronavirus/'
+else:
+    url = f'https://www.worldometers.info/coronavirus/country/{check}'
+
+
+covidInquiry(url)
+
+
+
+
+
